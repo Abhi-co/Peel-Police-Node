d1110803a9cb701e4dccd992a21914a4a00aa9eb
Prasann Joshi <prjoshi@buchanan.com>
2024-05-21 16:04:24 +0530

    This commit is after call stored procedure

--- src/database.service.ts
+++ src/database.service.ts
@@ -6,7 +6,7 @@ import * as sql from 'mssql';
 export class DatabaseService implements OnModuleInit {
   private pool: sql.ConnectionPool;
 
-  constructor(private configService: ConfigService) {}
+  constructor(private configService: ConfigService) { }
 
   async onModuleInit() {
     // await this.connect();
@@ -14,7 +14,7 @@ export class DatabaseService implements OnModuleInit {
 
   public async connect() {
     const dbConfig = this.configService.get('database');
-    console.log('dbconfig',dbConfig)
+    console.log('dbconfig', dbConfig)
     try {
       this.pool = await sql.connect(dbConfig);
       console.log('Connected to the database.');
@@ -24,22 +24,14 @@ export class DatabaseService implements OnModuleInit {
     return this.pool;
   }
 
-  // async query(query: string) {
-  //   try {
-  //     const result = await this.pool.request().query(query);
-  //     console.log('result',result)
-  //     return result.recordset;
-  //   } catch (error) {
-  //     console.error('Query error:', error);
-  //     throw error;
-  //   }
-  // }
-
   async getUserByUsername(username: string): Promise<any> {
     try {
-      const pool = await this.connect();     
-      const result = await pool.request().input('username', sql.VarChar, username).query('SELECT * FROM dbo.AdminMasterUser WHERE userName = @username');
-      console.log("recordset[0]:",result.recordset[0]);
+      const pool = await this.connect();
+      const result = await pool.request()
+      .input('username', sql.VarChar, username)
+      .execute('dbo.LoginValidateAdminByUsername');
+
+      console.log("recordset[0]:", result.recordset[0]);
 
       return result.recordset[0]; // Assuming username is unique, so only one record is expected
 
@@ -49,16 +41,5 @@ export class DatabaseService implements OnModuleInit {
       await this.pool.close(); // Make sure to release the connection back to the pool
     }
   }
-
-
-  // async getAllUsers(): Promise<any[]> {
-  //   try {
-  //     const pool = await this.pool.connect();
-  //     const result = await pool.request().query('SELECT * FROM dbo.AdminMasterUser');
-  //     return result.recordset;
-  //   } catch (error) {
-  //     throw new Error('Error retrieving users from database: ' + error.message);
-  //   }
-  // }
 
 }
